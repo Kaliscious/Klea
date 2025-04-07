60689c4b03c8eeee05cf80cdd23482bb185d2bba
Kaliscious <Kahlildienzo31@gmail.com>
2025-04-07 12:30:39 +0800

    Create Gallery.tsx

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Image } from 'lucide-react';
+import '../styles/gallery.css';
+
+const Gallery = () => {
+  // ... keep existing code
+};
+
+export default Gallery;
